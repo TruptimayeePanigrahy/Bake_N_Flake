44cf7dd0b6de9f8750e5ce08d3000f4f6b5b6228
Your Name <panigarhydeepakkumar27@gmail.com>
2023-05-08 19:53:59 +0530

    c

--- backend/routes/User.routes.js
+++ backend/routes/User.routes.js
@@ -26,6 +26,8 @@ userRouter.post("/register",async(req,res)=>{
                 res.send({"msg":"New Users has been registred"})
            }
         });
+
+
        
     }catch(err){
         res.send({"msg":"Something went wrong","error":err.message})
@@ -107,22 +109,22 @@ userRouter.get("/logout",(req,res)=>{
 
 
 
-// userRouter.post("/forgetpassword",async(req,res)=>{
-//     const {email}=req.body
-// try {
-//     const olduser=await UserModel.findOne({email})
-// if(!olduser){
-//     return res.send("user not presnt")
-// }
+userRouter.post("/forgetpassword",async(req,res)=>{
+    const {email}=req.body
+try {
+    const olduser=await UserModel.findOne({email})
+if(!olduser){
+    return res.send("user not presnt")
+}
 
-// const secret=process.env.secretekey +olduser.pass
-// const token=jwt.sign({email:olduser.email,id:olduser._id},secret,{expiresIn:"1d"})
+const secret=process.env.secretekey +olduser.pass
+const token=jwt.sign({email:olduser.email,id:olduser._id},secret,{expiresIn:"1d"})
 
 
-// } catch (error) {
+} catch (error) {
     
-// }
-// })
+}
+})
 
 
 
